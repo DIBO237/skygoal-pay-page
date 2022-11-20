284d29a1047d762fc10b6115984f98cb2075998d
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2022-11-20 22:40:45 +0530

    logo center

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
   return (
     <Navbar className='header mb-5' expand="lg" variant="light" bg="light">
       <Container>
-      <Navbar.Brand href="#"><img src='assets/skygoal-Logo 2.png' width={80} /></Navbar.Brand>
+      <Navbar.Brand href="#" className='mx-auto'><img src='assets/skygoal-Logo 2.png' width={80} /></Navbar.Brand>
       </Container>
       
    
