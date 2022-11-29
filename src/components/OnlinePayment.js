df4c83e21152176825e752c924037c13ff34c7b1
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2022-11-29 16:36:07 +0530

    cashfree added

--- src/components/OnlinePayment.js
+++ src/components/OnlinePayment.js
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useRef } from "react";
 import { Col, Button, Row, Form, InputGroup } from "react-bootstrap";
 import "../css/bank.css";
 import QRCode from "qrcode";
 import { FaCopy, FaShareAlt } from "react-icons/fa";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import { success_notify, fail } from "../utils/Utilfunctions";
+import { success_notify, fail, fail_notify } from "../utils/Utilfunctions";
 import { RWebShare } from "react-web-share";
-import _ from "lodash"
+import _ from "lodash";
+
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
 export default function OnlinePayment() {
+  const formEl = useRef();
   const [urls, setURl] = useState("");
 
   const generateURL = (upi, name) => {
@@ -31,56 +35,81 @@ export default function OnlinePayment() {
   const bank = [
     {
       id: 0,
-      AcName: "SKYGOAL INNOVA TECHNOLOGIES PVT LTD",
-      acNum: "180305500728",
-      ifsc: "ICIC0001803",
-      branch: "ICICI BANK LTD, ELURU ROAD VIJAYAWADA",
-      gstNum: "36ABACS8072J1Z0",
+      name:"cashfree",
+      url:"http://localhost:4000/cashfree-pay",
       imgUrl: "assets/cash.svg",
     },
     {
       id: 1,
-      AcName: "SKYGOAL INNOVA TECHNOLOGIES PVT LTD",
-      acNum: "10075085892",
-      ifsc: "IDFB0080391",
-      branch: "Vijayawada Branch",
-      gstNum: "36ABACS8072J1Z0",
+      name:"Payu",
+      url:"http://localhost:4000/payu-pay",
       imgUrl: "assets/cash1.svg",
-    }
+    },
   ];
 
   const [selectBank, setBank] = useState(0);
   const [bankDetails, setDetails] = useState(bank[selectBank]);
 
-  const [name,setName]=useState("")
-  const [phone,setPhone]=useState("")
-  const [email,setEmail]=useState("")
-  const [amount,setAmount]=useState(0)
-  const [remarks,setRemarks]=useState("")
-  const [gst,setGst]=useState("")
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [remarks, setRemarks] = useState("");
+  const [gst, setGst] = useState("");
   const [convienceFee, setConviencefee] = useState(0);
   const [amtTotal, setTotal] = useState(0);
 
+  const submitPay = async (e) => {
+    
+    if (_.isEmpty(name)) {
+      return fail_notify("Name is required !", 3000);
+    }
+    
+    if (_.isEmpty(phone)) {
+
+      return fail_notify("Mobile number is required !", 3000);
+
+    }
+    if (_.isEmpty(email)) {
+      return fail_notify("Email is required !", 3000);
+    }
+    if (_.isEmpty(amount)) {
+      return fail_notify("Please Enter an amount", 3000);
+    }
+    let datas = {
+     remarks: remarks,
+     custGst:gst
+      
+    };
+
+    
+    try{
+      formEl.current.custDetails.value = JSON.stringify(datas);
+      formEl.current && formEl.current.submit();
+    }catch(err){
+
+       fail_notify("Something went wrong try again later",4000)
+    }
+    
+
+
+  };
+
   //AMOUNT HANDLER
-  const AmountHandle = (e)=>{
+  const AmountHandle = (e) => {
+    setAmount(e);
 
-    setAmount(e)
+    if (!_.isEmpty(e)) {
+      setConviencefee(((2 / 100) * e).toFixed(2));
+      console.log(((2 / 100) * e).toFixed(2));
+      let fee = ((2 / 100) * e).toFixed(2);
+    }
 
-    if(!_.isEmpty(e)){
-      setConviencefee(((2/ 100) * e).toFixed(2))
-      console.log(((2/ 100) * e).toFixed(2))
-      let fee = ((2/ 100) * e).toFixed(2)
-      
+    if (_.isEmpty(e)) {
+      setConviencefee(0);
+      setTotal(0);
     }
-   
-    if(_.isEmpty(e)){
-      
-      setConviencefee(0)
-      setTotal(0)
-    }
-    
-    
-  }
+  };
   const handler = (data) => {
     setBank(data);
     setDetails(bank[data]);
@@ -95,7 +124,6 @@ export default function OnlinePayment() {
   }, [selectBank]);
   return (
     <div>
-      
       <Row>
         <Col
           className="mb-5 h-100 mx-auto"
@@ -107,8 +135,6 @@ export default function OnlinePayment() {
         >
           <div className="box-outer">
             <div className="box-inner">
-             
-
               <div>
                 <h5>Enter Details</h5>
               </div>
@@ -123,25 +149,24 @@ export default function OnlinePayment() {
                       aria-describedby="basic-addon2"
                       value={name}
                       type="text"
-                      
+                      onChange={(e) => setName(e.target.value)}
                       className="form-field"
                     />
-                   
                   </InputGroup>
                 </Form.Group>
                 {/* // INPUT GROUPS */}
                 <Form.Group className="mb-3">
                   <Form.Label>Mobile number*</Form.Label>
                   <InputGroup className="mb-3">
-                    <Form.Control
-                      placeholder="Recipient's Phone number"
-                      aria-label="Recipient's username"
-                      aria-describedby="basic-addon2"
+                    <PhoneInput
+                      country={"in"}
                       value={phone}
-                      type="text"
-                      className="form-field"
+                      onChange={(e) => setPhone(e)}
+                      className=""
+                      
+                      inputClass="form-field w-100"
+                      containerClass="W-100"
                     />
-                   
                   </InputGroup>
                 </Form.Group>
                 {/* // INPUT GROUPS */}
@@ -153,26 +178,25 @@ export default function OnlinePayment() {
                       aria-label="Recipient's username"
                       aria-describedby="basic-addon2"
                       value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                       type="email"
                       className="form-field"
                     />
-                  
                   </InputGroup>
                 </Form.Group>
-                 {/* // INPUT GROUPS */}
-                 <Form.Group className="mb-3">
+                {/* // INPUT GROUPS */}
+                <Form.Group className="mb-3">
                   <Form.Label>Amount*</Form.Label>
                   <InputGroup className="mb-3">
                     <Form.Control
                       placeholder="Enter your Amount"
                       aria-label="Recipient's username"
                       aria-describedby="basic-addon2"
-                      onChange={(e)=>AmountHandle(e.target.value)}
+                      onChange={(e) => AmountHandle(e.target.value)}
                       value={amount}
                       type="number"
                       className="form-field"
                     />
-                  
                   </InputGroup>
                 </Form.Group>
                 {/* // INPUT GROUPS */}
@@ -184,10 +208,10 @@ export default function OnlinePayment() {
                       aria-label="Recipient's username"
                       aria-describedby="basic-addon2"
                       value={remarks}
+                      onChange={(e) => setRemarks(e.target.value)}
                       type="text"
                       className="form-field"
                     />
-                  
                   </InputGroup>
                 </Form.Group>
                 {/* // INPUT GROUPS */}
@@ -199,10 +223,10 @@ export default function OnlinePayment() {
                       aria-label="Recipient's username"
                       aria-describedby="basic-addon2"
                       value={gst}
+                      onChange={(e) => setGst(e.target.value)}
                       type="text"
                       className="form-field"
                     />
-                   
                   </InputGroup>
                 </Form.Group>
               </Form>
@@ -222,56 +246,107 @@ export default function OnlinePayment() {
               <div className=" mb-3">
                 <h5>Payment Details</h5>
               </div>
-             <div>
-               <div style={{display:"flex",justifyContent:"space-between"}}>
-                 <p>Amount</p>
-                 <p>&#8377;{amount}</p>
-               </div>
-               <div style={{display:"flex",justifyContent:"space-between"}}>
-                 <p>Convinence Fees (2%):</p>
-                 <p>&#8377;{convienceFee}</p>
-               </div>
-               <hr></hr>
-               <div style={{display:"flex",justifyContent:"space-between"}}>
-                 <p>Total Amount:</p>
-                 <p>&#8377;{amount ? parseInt(convienceFee )+ parseInt(amount): 0}</p>
-               </div>
-             </div>
-             <div>
-              <p style={{color:"red",fontWeight:"bold"}}>NOTE: To avoid convinence fee use Bank Transfer</p>
-             </div>
-             {/* DYNAMIC PAYMENT SECTION */}
-             <div>
-             <div className="px-3 py-3">
-        <Form>
-          {/* SWITHABLE FORM DATA BANK */}
-          {bank.map((data) => (
-            <Form.Check className="mb-3"  type={"radio"} name="group1">
-              <div
-                className=""
-                style={{ display: "flex", justifyContent: "" }}
-              >
-                <Form.Check.Input
-                  value="bank"
-                  className="mt-3"
-                  onChange={() => handler(data.id)}
-                  checked={selectBank === data.id}
-                  type={"radio"}
-                />
-                <div className="" style={{ display: "flex", justifyContent:"space-around",flexDirection:"row", paddingLeft:5 }}>
-                  <img className="img-fluid mt-2" src={data.imgUrl} width={70} />
+              <div>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <p>Amount</p>
+                  <p>&#8377;{amount ? amount : 0}</p>
+                </div>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <p>Convinence Fees (2%):</p>
+                  <p>&#8377;{convienceFee}</p>
+                </div>
+                <hr></hr>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <p>Total Amount:</p>
+                  <p>
+                    &#8377;
+                    {amount ? parseInt(convienceFee) + parseInt(amount) : 0}
+                  </p>
                 </div>
               </div>
-            </Form.Check>
-          ))}
+              <div>
+                <p style={{ color: "red", fontWeight: "bold" }}>
+                  NOTE: To avoid convinence fee use Bank Transfer
+                </p>
+              </div>
+              {/* DYNAMIC PAYMENT SECTION */}
+              <div>
+                <div className="px-3 py-3">
+                  <Form>
+                    {/* SWITHABLE FORM DATA BANK */}
+                    {bank.map((data) => (
+                      <Form.Check className="mb-3" type={"radio"} name="group1">
+                        <div
+                          className=""
+                          style={{ display: "flex", justifyContent: "" }}
+                        >
+                          <Form.Check.Input
+                            value="bank"
+                            className="mt-3"
+                            onChange={() => handler(data.id)}
+                            checked={selectBank === data.id}
+                            type={"radio"}
+                          />
+                          <div
+                            className=""
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-around",
+                              flexDirection: "row",
+                              paddingLeft: 5,
+                            }}
+                          >
+                            <img
+                              className="img-fluid mt-2"
+                              src={data.imgUrl}
+                              width={70}
+                            />
+                          </div>
+                        </div>
+                      </Form.Check>
+                    ))}
 
-          <div className="mt-5">
-             <Button className={amount >0 ? "py-2" : "disabled py-2"} style={{width:"100%",backgroundColor:"#2A2742"}}>PAY &#8377;{amount ? parseInt(convienceFee )+ parseInt(amount): 0}</Button>
-          </div>
-        </Form>
+                    <div className="mt-5">
+                      <Button
+                        className={amount > 0 ? "py-2" : "disabled py-2"}
+                        onClick={(e) => submitPay(e)}
+                        style={{ width: "100%", backgroundColor: "#2A2742" }}
+                      >
+                        PAY &#8377;
+                        {amount ? parseInt(convienceFee) + parseInt(amount) : 0}
+                      </Button>
+                    </div>
+                  </Form>
+                </div>
+              </div>
+
+              <div>
+      
+          <Form
+            className="d-none"
+            name="form"
+            ref={formEl}
+            id="x1"
+            method="POST"
+            action={bankDetails.url}
+            //action="http://localhost:4000/paynow"
+          >
+            
+            <input type="text" name="name" value={name}></input>
+            <input type="email" name="email" value={email}></input>
+            <input type="text" name="phone" value={phone}></input>
+            <input type="text" name="amount" value={amount ? parseInt(convienceFee )+ parseInt(amount): 1}></input>
+            <input type="text" name="gatewaytype" value={bankDetails.name}></input>
+            <input type="text" name="custDetails" value={""}></input>
+          </Form>
+      
       </div>
-             </div>
-              
             </div>
           </div>
         </Col>
